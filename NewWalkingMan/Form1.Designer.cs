b0124e1365d2e78951533c16d624cbfe34c110b5
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-15 10:12:20 -0400

    Initial commit

---
+++ NewWalkingMan/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NewWalkingMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(224, 24);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(332, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Click start to begin. Afterwards click on a picture to start to animation.";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(359, 53);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 558);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "Form1";
+            this.Text = "New Walking Man";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ NewWalkingMan/Form1.cs
@@ -0,0 +1,143 @@
+﻿/*
+ * Created by: Jadon F
+ * Created on: 14-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - Dynamic Picture Boxes
+ * This program creates dynamic picture boxes.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Add this library
+using System.Threading;
+
+
+namespace NewWalkingMan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePictureBoxes()
+        {
+            GeneratePicBox(61, 78);
+            GeneratePicBox(464, 78);
+            GeneratePicBox(61, 358);
+            GeneratePicBox(464, 358);
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GeneratePictureBoxes();
+        }
+
+        private void GeneratePicBox(int x, int y)
+        {
+            //dynamic
+            PictureBox tmpPicMan = new PictureBox();
+            Point pntPic = new System.Drawing.Point(x, y);
+
+            //location
+            tmpPicMan.Location = pntPic;
+
+            //image
+            tmpPicMan.Image = Properties.Resources.walk1;
+
+            //stretch
+            tmpPicMan.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //size
+            tmpPicMan.ClientSize = new Size(Properties.Resources.walk1.Width, Properties.Resources.walk1.Height);
+
+            //click
+            tmpPicMan.Click += new System.EventHandler(PictureBox_Click);
+
+            //add
+            Controls.Add(tmpPicMan);
+        }
+
+        private void PictureBox_Click (object sender, EventArgs e)
+        {
+            PictureBox tmpPicMan = (PictureBox)sender;
+
+            //Define Variables
+            const byte MAX_FRAMES = 10;
+            int Counter = 1;
+
+            //Set the loop
+            while (Counter < MAX_FRAMES + 1)
+            {
+                if (Counter == 1)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk1;
+                }
+
+                else if (Counter == 2)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk2;
+                }
+
+                else if (Counter == 3)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk3;
+                }
+
+                else if (Counter == 4)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk4;
+                }
+
+                else if (Counter == 5)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk5;
+                }
+
+                else if (Counter == 6)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk6;
+                }
+
+                else if (Counter == 7)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk7;
+                }
+
+                else if (Counter == 8)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk8;
+                }
+
+                else if (Counter == 9)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk9;
+                }
+
+                else if (Counter == 10)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk10;
+                    Counter = 1;
+                }
+
+                //Increment the counter
+                Counter = Counter + 1;
+
+                //Refresh and set intrevals
+                this.Refresh();
+                Thread.Sleep(100);
+            }
+
+            }
+
+
+
+    }
+}
